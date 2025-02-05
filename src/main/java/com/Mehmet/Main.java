3ed4bfc318f33de29d758a2c18384cb0e71ebd60
Mehmet Karagülle <fatih58kg@gmail.com>
2025-02-05 17:20:19 +0300

    java core init

--- src/main/java/com/Mehmet/Main.java
+++ src/main/java/com/Mehmet/Main.java
@@ -1,9 +1,27 @@
 package com.Mehmet;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.security.MessageDigest;
+import java.nio.charset.StandardCharsets;
+import org.bouncycastle.util.encoders.Hex;
+
 public class Main {
     public static void main(String[] args) {
+        SHA2 sha2 = new SHA2();
+        String hashedValue = sha2.digest("Merhaba", "SHA-256");
+        System.out.println("Şifrelenmiş metin: " + hashedValue);
+    }
+}
 
+class SHA2 {
+    public String digest(String text, String functionValue) {
+        try {
+            MessageDigest digestObject = MessageDigest.getInstance(functionValue);
+            digestObject.update(text.getBytes(StandardCharsets.UTF_8));
+            byte[] digestBytes = digestObject.digest();
+            return new String(Hex.encode(digestBytes));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
